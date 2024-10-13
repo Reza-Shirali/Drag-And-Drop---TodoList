85e64517714e2454a4ebc1ca4d87bcd057a5a15a
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-10-13 11:21:05 +0330

    finish project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import { FaGithub } from "react-icons/fa";
 function Header() {
   return (
     <>
-      <header className="my-6 flex justify-between items-center w-[60%] border-2 p-4 rounded-2xl">
+      <header className="my-6 flex justify-between items-center w-[60%] border-2 p-6 rounded-2xl">
         <div className="flex gap-4 justify-center items-center">
-          <h1 className="text-2xl font-bold text-white select-none">
+          <h1 className="text-3xl font-bold text-white select-none">
             RezaShirali - ReactJS
           </h1>
           <a href="https://github.com/Reza-Shirali" className="text-white text-5xl" target="_blanc"><FaGithub /></a>
         </div>
         <div>
-          <h3 className="text-2xl font-bold text-white select-none">Todo List - Drag And Drop</h3>
+          <h3 className="text-3xl font-bold text-white select-none">Todo List - Drag And Drop</h3>
         </div>
       </header>
     </>
